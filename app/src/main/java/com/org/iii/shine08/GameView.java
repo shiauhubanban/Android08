903e9fe511fc0a46d4c1ec64bf2bb104e6375e5d
user <shiauhubanban>
2016-11-18 10:42:47 +0800

    v5

--- app/src/main/java/com/org/iii/shine08/GameView.java
+++ app/src/main/java/com/org/iii/shine08/GameView.java
@@ -8,21 +8,37 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class GameView extends View{
     private Resources res;
+    private Context context;
+    private int viewH,viewW;
+    private boolean isInit;
+    private Bitmap bmpBaill;
 
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.context = context;
         res = context.getResources();
+
+
         //setBackgroundColor(Color.YELLOW);
+    }
+    private void init(){
+        viewH =getHeight();viewW=getWidth();
+        Log.v("shine",viewW +"x" + viewH);
+        bmpBaill = BitmapFactory.decodeResource(res,R.drawable.b1);
+        isInit = true;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Bitmap ball = BitmapFactory.decodeResource(res,R.drawable.b1);
-        canvas.drawBitmap(ball,0,0,null);
+        if(!isInit) init();
+
+        canvas.drawBitmap(bmpBaill,0,0,null);  //畫個ball
+
     }
 }
