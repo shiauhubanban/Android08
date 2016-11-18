ba403876a3731d3164b4c05338f83c4057bb8d33
user <shiauhubanban>
2016-11-18 11:23:00 +0800

    v6

--- app/src/main/java/com/org/iii/shine08/GameView.java
+++ app/src/main/java/com/org/iii/shine08/GameView.java
@@ -7,6 +7,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -16,20 +17,28 @@ public class GameView extends View{
     private Context context;
     private int viewH,viewW;
     private boolean isInit;
-    private Bitmap bmpBaill;
+    private Bitmap bmpBall;
+    private Matrix matrix;
+    private float ballW,ballH;
 
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.context = context;
         res = context.getResources();
-
+        matrix = new Matrix();
 
         //setBackgroundColor(Color.YELLOW);
     }
     private void init(){
         viewH =getHeight();viewW=getWidth();
         Log.v("shine",viewW +"x" + viewH);
-        bmpBaill = BitmapFactory.decodeResource(res,R.drawable.b1);
+        bmpBall = BitmapFactory.decodeResource(res,R.drawable.b1);
+        ballW = viewW/12f; ballH = ballW;
+
+        matrix.postScale(ballW/bmpBall.getWidth(),ballH/bmpBall.getHeight());
+        matrix.postScale(ballW/bmpBall.getWidth(), ballH/bmpBall.getHeight());
+        bmpBall = Bitmap.createBitmap(bmpBall,0,0,bmpBall.getWidth(),bmpBall.getHeight(),matrix,false);
+
         isInit = true;
     }
 
@@ -38,7 +47,7 @@ public class GameView extends View{
         super.onDraw(canvas);
         if(!isInit) init();
 
-        canvas.drawBitmap(bmpBaill,0,0,null);  //畫個ball
+        canvas.drawBitmap(bmpBall,0,0,null);  //畫個ball
 
     }
 }
