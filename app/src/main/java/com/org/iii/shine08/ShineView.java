ddf68db1cf166169b6b5ce1c5f1d9de4425fafaa
user <shiauhubanban>
2016-11-18 09:33:29 +0800

    v2

---
+++ app/src/main/java/com/org/iii/shine08/ShineView.java
@@ -0,0 +1,17 @@
+package com.org.iii.shine08;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class ShineView extends View{
+    public ShineView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
