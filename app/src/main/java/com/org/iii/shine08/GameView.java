63a6c85beaf40d69818f6ad6fe1986da456a3303
user <shiauhubanban>
2016-11-18 09:23:55 +0800

    v1

---
+++ app/src/main/java/com/org/iii/shine08/GameView.java
@@ -0,0 +1,18 @@
+package com.org.iii.shine08;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class GameView extends View{
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
